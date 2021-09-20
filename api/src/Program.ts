7ab5c14d1fb59b4b6ef34fd37e5f99ac2895dff4
exromany <exromany@gmail.com>
2021-09-20 12:44:20 +0300

    feat: program subscription

--- api/src/Program.ts
+++ api/src/Program.ts
@@ -1,4 +1,11 @@
-import { PublicKey, Connection, GetProgramAccountsConfig, Commitment } from '@solana/web3.js';
+import {
+  PublicKey,
+  Connection,
+  GetProgramAccountsConfig,
+  Commitment,
+  GetProgramAccountsFilter,
+  Context,
+} from '@solana/web3.js';
 import { Account } from './Account';
 import { Buffer } from 'buffer';
 
@@ -15,6 +22,23 @@ export abstract class Program {
   ) {
     return (await connection.getProgramAccounts(this.PUBKEY, configOrCommitment)).map(
       ({ pubkey, account }) => new Account(pubkey, account),
+    );
+  }
+
+  static async getSubscription(
+    connection: Connection,
+    cb: (data: { account: Account; context: Context }) => void,
+    commitment?: Commitment,
+    filters?: GetProgramAccountsFilter[],
+  ) {
+    return connection.onProgramAccountChange(
+      this.PUBKEY,
+      ({ accountId, accountInfo }, context) => {
+        const account = new Account(accountId, accountInfo);
+        cb({ account, context });
+      },
+      commitment,
+      filters,
     );
   }
 }
