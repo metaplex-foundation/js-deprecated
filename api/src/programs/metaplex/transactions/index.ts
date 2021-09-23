8da7b3d14805109c90169dac8e9d9c3b71f12fdf
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2021-09-23 13:24:12 +0300

    fix: missing exports

--- api/src/programs/metaplex/transactions/index.ts
+++ api/src/programs/metaplex/transactions/index.ts
@@ -2,3 +2,5 @@ export * from './SetStore';
 export * from './SetWhitelistedCreator';
 export * from './StartAuction';
 export * from './InitAuctionManagerV2';
+export * from './ClaimBid';
+export * from './RedeemBid';
