2261b1b828af9bdbb0c53a096a1dd96d60a4dda1
Paul Kurochka <kurochka.p@gmail.com>
2021-10-20 12:29:48 +0300

    refactor: add account type in root export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './providers';
 export * from './Connection';
 export * from './wallet';
+export * from './Account';
 export * as utils from './utils';
 export * as errors from './errors';
 export * as actions from './actions';
