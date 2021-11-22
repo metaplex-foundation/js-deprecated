f2e4100a7b43a63a66a59fad90fe7ab3a4bca97c
Paul Kurochka <kurochka.p@gmail.com>
2021-11-22 17:13:44 +0200

    feat: add burn token action

---
+++ src/actions/burnToken.ts
@@ -0,0 +1,36 @@
+import { PublicKey } from '@solana/web3.js';
+import { Wallet } from '../wallet';
+import { Connection } from '../Connection';
+import { sendTransaction } from './transactions';
+import { Transaction } from '../Transaction';
+import { Token, TOKEN_PROGRAM_ID, u64 } from '@solana/spl-token';
+
+interface IBurnTokenParams {
+  connection: Connection;
+  wallet: Wallet;
+  token: PublicKey;
+  mint: PublicKey;
+  amount: number | u64;
+  owner?: PublicKey;
+}
+
+interface IBurnTokenResponse {
+  txId: string;
+}
+
+export const burnToken = async ({
+  connection,
+  wallet,
+  token,
+  mint,
+  amount,
+  owner
+}: IBurnTokenParams): Promise<IBurnTokenResponse> => {
+  const tx = new Transaction({ feePayer: wallet.publicKey }).add(
+    Token.createBurnInstruction(TOKEN_PROGRAM_ID, mint, token, owner ?? wallet.publicKey, [], amount),
+  );
+
+  const txId = await sendTransaction({ connection, wallet, txs: [tx] });
+
+  return { txId };
+};
