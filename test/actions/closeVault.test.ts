e35e5fcb656f79ac10a84d268e833fddf3c167d2
Valentine Panchin <vpanchyn@markerr.com>
2021-12-01 18:15:46 +0200

    feat: close vault action
    Checklist update
    feat: close vault action
    
    update tests
    fix: lint fix

--- test/actions/closeVault.test.ts
+++ test/actions/closeVault.test.ts
@@ -1,24 +1,26 @@
 import { NATIVE_MINT } from '@solana/spl-token';
 import { Connection, NodeWallet } from '../../src';
-import { createVault } from '../../src/actions';
+import { createVault, closeVault } from '../../src/actions';
 import { FEE_PAYER, pause, VAULT_EXTENRNAL_PRICE_ACCOUNT } from '../utils';
 import { mockAxios200 } from './shared';
 import { Vault } from '../../src/programs/vault';
 
 jest.mock('axios');
 
-describe('creating a Vault', () => {
+describe('closing a Vault', () => {
   const connection = new Connection('devnet');
   const wallet = new NodeWallet(FEE_PAYER);
 
-  jest.setTimeout(30000);
+  // console.log(FEE_PAYER.publicKey.toString());
+  jest.setTimeout(60000);
 
   describe('success', () => {
     beforeEach(() => {
       mockAxios200(wallet);
     });
 
-    test('generates vault', async () => {
+    test('closes vault', async () => {
+      let vault;
       const vaultResponse = await createVault({
         connection,
         wallet,
@@ -27,9 +29,23 @@ describe('creating a Vault', () => {
       });
 
       await pause(20000);
-      const vault = await Vault.load(connection, vaultResponse.vault);
+      vault = await Vault.load(connection, vaultResponse.vault);
       expect(vault).toHaveProperty('data');
       expect(vault.data.state).toEqual(0);
+
+      closeVault({
+        connection,
+        wallet,
+        ...vaultResponse,
+        priceMint: NATIVE_MINT,
+        externalPriceAccount: VAULT_EXTENRNAL_PRICE_ACCOUNT,
+      });
+
+      await pause(20000);
+
+      vault = await Vault.load(connection, vaultResponse.vault);
+      expect(vault).toHaveProperty('data');
+      expect(vault.data.state).toEqual(2);
     });
   });
 });
