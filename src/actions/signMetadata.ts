2a504ecb8fa82d4b0d7e306917c6ec6b344d6dee
Danny Povolotski <dannypovolotski@gmail.com>
2022-01-22 21:15:43 +0300

    docs: signMetadata

--- src/actions/signMetadata.ts
+++ src/actions/signMetadata.ts
@@ -3,13 +3,23 @@ import { Wallet } from '../wallet';
 import { Metadata, SignMetadata } from '@metaplex-foundation/mpl-token-metadata';
 import { sendTransaction } from './transactions';
 
+/**
+ * Parameters for {@link signMetadata}
+ */
 export interface SignMetadataParams {
   connection: Connection;
+  /** Will be used as both the fee payer and {@link signer} if no separate {@link signer} was otherwise specified. If used as a signer, the wallet address must be included in the `creators` array. **/
   wallet: Wallet;
+  /** Mint address for the token associated with the {@link Metadata} account **/
   editionMint: PublicKey;
+  /** An optional signer. If specified, the signer address must be included in the `creators` array on the {@link Metadata} account data **/
   signer?: Keypair;
 }
 
+/**
+ * Sign a MetaData account that has the provided wallet as an unverified creator so that it is now verified.
+ * @return This action returns the resulting transaction id once it has been executed
+ */
 export const signMetadata = async (
   { connection, wallet, editionMint, signer } = {} as SignMetadataParams,
 ): Promise<string> => {
